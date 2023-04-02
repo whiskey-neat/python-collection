27b80d88b52f60200f5591cc93f7715d7caff6ff
whiskey-neat <84135221+whiskey-neat@users.noreply.github.com>
2023-04-02 13:59:11 +0100

    chat app file creation

--- chat_app.py
+++ chat_app.py
@@ -1,1 +1,18 @@
 # Section D: Chat App (10 Marks)
+# main menu for Chat App
+
+import os
+
+while True:
+    select = input("1. Start the Server\n"
+                   "2. Chat\n"
+                   "3. Exit\n"
+                   "Select an option: ")
+    select = int(select)
+
+    if select == 1:
+        os.system("start cmd /k server.py")
+    if select == 2:
+        os.system("start cmd /k client.py")
+    if select == 3:
+        quit()
